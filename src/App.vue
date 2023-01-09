a3ff0fedd5469ba565d1366a39cd55982e37b37b
francescotrudu <francescotrudu84@gmail.com>
2023-01-09 23:30:29 +0100

    Setup Vite + Vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import HeaderVue from "./components/HeaderVue.vue";
+import MainVue from "./components/MainVue.vue";
+import FooterVue from "./components/FooterVue.vue";
+
+export default {
+  components: {
+    HeaderVue,
+    MainVue,
+    FooterVue
+  }
+}
+</script>
+
+<template>
+  <HeaderVue />
+  <MainVue />
+  <FooterVue />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/FooterVue.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <footer>
+        <h1>footer</h1>
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="" scoped>
+    
+</style>
