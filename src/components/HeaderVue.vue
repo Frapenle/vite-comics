19c4f6be129f0d46a22ae0f25a174f309b3cc66a
francescotrudu <francescotrudu84@gmail.com>
2023-01-10 00:11:39 +0100

    Add navHeader array of objects

--- src/components/HeaderVue.vue
+++ src/components/HeaderVue.vue
@@ -1,23 +1,78 @@
 <template>
     <header>
-        <img src="../assets/img/dc-logo.png" alt="DC logo">
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-        </ul>
+        <nav>
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="DC logo">
+            </div>
+            <ul>
+                <li>menu 1</li>
+                <li>menu 2</li>
+                <li>menu 3</li>
+                <li>menu 4</li>
+            </ul>
+        </nav>
     </header>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            navHeader: [
+                {
+                    name: "characters",
+                    active: false
+                },
+                {
+                    name: "comics",
+                    active: true
+                },
+                {
+                    name: "movies",
+                    active: false
+                },
+                {
+                    name: "tv",
+                    active: false
+                },
+                {
+                    name: "games",
+                    active: false
+                },
+                {
+                    name: "collectibles",
+                    active: false
+                },
+                {
+                    name: "videos",
+                    active: false
+                },
+                {
+                    name: "fans",
+                    active: false
+                },
+                {
+                    name: "news",
+                    active: false
+                },
+                {
+                    name: "shop",
+                    active: false
+                },
+            ]
+        }
+    },
 }
 </script>
 
 <style scoped>
-header {
+nav {
     display: flex;
+    justify-content: space-between;
+}
+
+ul {
+    display: flex;
+    gap: 2rem;
 }
 </style>
