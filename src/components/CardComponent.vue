760df996929b9ee7d84531efc74a8d972be15e0e
francescotrudu <francescotrudu84@gmail.com>
2023-01-10 17:07:08 +0100

    Add cardComponent

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,25 @@
+<template lang="">
+    <div class="card">
+
+    </div>
+</template>
+<script>
+
+export default {
+    name: 'CardComponent',
+    props: [
+        "imgPath",
+        "price",
+        "serie",
+        "type"
+    ]
+}
+</script>
+<style lang="scss">
+.card {
+    height: 100px;
+    width: calc(100% / 4);
+    border: 2px solid red;
+    padding: 2rem;
+}
+</style>
