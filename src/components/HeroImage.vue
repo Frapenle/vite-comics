52166de0a845954b8b98843b54485a3734103743
francescotrudu <francescotrudu84@gmail.com>
2023-01-10 18:43:32 +0100

    Add jumbotrone component

---
+++ src/components/HeroImage.vue
@@ -0,0 +1,31 @@
+<template lang="">
+    <div class="jumbo">
+        <img src="../assets/img/jumbotron.jpg" alt="">
+        <div class="container">
+            <div class="current-series">
+                <h4>current series</h4>
+            </div>
+        </div>
+    </div>
+
+
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+@use '../styles/partials/variables' as *;
+
+.jumbo {
+    height: 300px;
+    overflow: hidden;
+}
+
+.current-series {
+    padding: 1rem;
+    color: white;
+    background-color: $main-color;
+}
+</style>
