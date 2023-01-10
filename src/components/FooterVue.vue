6658bf55bd0b6d385a7b4221a5a0f439d87253e6
francescotrudu <francescotrudu84@gmail.com>
2023-01-10 15:52:39 +0100

    Add footer lists

--- src/components/FooterVue.vue
+++ src/components/FooterVue.vue
@@ -1,6 +1,60 @@
 <template lang="">
     <footer>
-        <h1>footer</h1>
+        <div class="container">
+
+        
+            <section class="footer-top">
+                <nav>
+                    <!-- first column list -->
+                    <ul>
+                        <li><h3>DC Comics</h3></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                        <!--  -->
+                        <li><h3>Shop</h3></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                    </ul>
+
+                    <!-- second column list -->
+                    <ul>
+                        <li><h3>DC Comics</h3></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+
+                    <!-- third column list -->
+                    <ul>
+                        <li><h3>DC Comics</h3></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+
+
+
+                </nav>
+            </section>
+            <section class="footer-down"></section>
+        </div>
     </footer>
 </template>
 
@@ -10,6 +64,31 @@ export default {
 }
 </script>
 
-<style lang="" scoped>
-    
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/general.scss' as *;
+
+footer {
+    background-color: grey;
+    padding: 2rem 0;
+}
+
+li {
+    color: white;
+
+    h3 {
+        padding: .8rem 0 .2rem;
+    }
+
+    & a {
+        color: white;
+        font-size: .8rem;
+        font-weight: 300;
+    }
+}
+
+nav {
+    display: flex;
+    gap: 4rem;
+}
 </style>
